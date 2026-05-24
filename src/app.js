4413a3dba6e4b69ad083a927e20f1916b6b31f1d
Julieta Soler <julieta_soler@hotmail.com>
2026-05-24 10:06:54 -0300

    Se reestructura carpetas y se configura para app.js para utilizar handlebars

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import handlebars from 'express-handlebars'
 
 const app = express()
 
 app.use(express.json())
-
 app.use(express.urlencoded({ extended: true }))
+
+app.engine('handlebars', handlebars.engine())
+app.set('view-engine', 'handlebars')
+app.set('views', `${process.cwd()}/src/views`)
 
 const PORT = 8080
 
