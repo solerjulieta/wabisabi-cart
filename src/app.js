cd28b94624ede91f238bcaadc839f62ce4841686
Julieta Soler <julieta_soler@hotmail.com>
2026-05-24 21:22:00 -0300

    Se agrega carpeta public, index.html y js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
 import multer from 'multer'
+import http from 'http'
+import { Server } from 'socket.io'
 
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('./public'))
 
 app.engine('handlebars', handlebars.engine())
 app.set('view-engine', 'handlebars')
