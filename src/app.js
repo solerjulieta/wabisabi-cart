3810939d50fb3c1246ca2cd62f458fbf8d472f41
Julieta Soler <julieta_soler@hotmail.com>
2026-05-29 08:58:43 -0300

    Se app.js, configuración de handlebarsx2

--- src/app.js
+++ src/app.js
@@ -6,8 +6,13 @@ import http from 'http'
 import { Server } from 'socket.io'
 import connectDB from './config/db.js'
 import router from './api/routes/routes.index.js'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 
 dotenv.config()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
 const server = http.createServer(app)
@@ -21,15 +26,15 @@ connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static('./public'))
+app.use(express.static(join(__dirname, '../public')))
 
 app.engine('handlebars', handlebars.engine({
-    layoutsDir: `${process.cwd()}/src/views/layouts`,
-    partialsDir: `${process.cwd()}/src/views/partials`,
+    layoutsDir: join(__dirname, 'views/layouts'),
+    partialsDir: join(__dirname, 'views/partials'),
     defaultLayout: 'main'
 }))
 app.set('view engine', 'handlebars')
-app.set('views', `${process.cwd()}/src/views`)
+app.set('views', join(__dirname, 'views'))
 
 app.use('/api', router)
 
